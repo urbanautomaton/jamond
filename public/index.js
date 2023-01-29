f931bc9ef440ff329ee6bf494b2da43c152cc80a
Simon Coffey <simon.coffey@futurelearn.com>
2023-01-29 22:19:23 +0000

    Use exponential fade-in/out to avoid nasty clicks
    Step changes in waveforms (e.g. if you suddenly start or stop one
    mid-phase) create high-frequency artifacts and are audible as clicks. By
    ramping to the desired value we can eliminate these.
    
    I'm not sure this is really needed for the start of a note since if it
    always starts at the zero-point, there'd be no discontinuity. But we'll
    need to look at attack later so why not.

--- public/index.js
+++ public/index.js
@@ -2,31 +2,38 @@ const audioContext = new AudioContext();
 const mainGainNode = audioContext.createGain();
 mainGainNode.connect(audioContext.destination);
 mainGainNode.gain.value = 1.0;
-let osc = null;
+let note = null;
 
 const aKey = document.getElementById("A");
 
 const playTone = () => {
-  console.log("Starting");
-  if (osc !== null) {
-    osc.stop();
+  if (note !== null) {
+    note.osc.stop();
   }
 
+  gainNode = audioContext.createGain();
+  gainNode.connect(mainGainNode);
+  gainNode.gain.value = 0;
+
   osc = audioContext.createOscillator();
-  osc.connect(mainGainNode);
-
+  osc.connect(gainNode);
   osc.type = "sine";
-
   osc.frequency.value = 220;
-  osc.start();
 
-  return osc;
+  osc.start();
+  gainNode.gain.setTargetAtTime(1.0, audioContext.currentTime, 0.005);
+
+  note = { osc, gainNode };
 };
 
 const stopTone = () => {
-  if (osc !== null) {
-    osc.stop();
-    osc = null;
+  if (note !== null) {
+    note.gainNode.gain.setTargetAtTime(0, audioContext.currentTime, 0.005);
+    let osc = note.osc;
+    setTimeout(() => {
+      osc.stop();
+    }, 100);
+    note = null;
   }
 };
 

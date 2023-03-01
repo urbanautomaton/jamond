f4b8d9a179c74aabd551567280b048abb4fb5879
Simon Coffey <simon.coffey@futurelearn.com>
2023-03-01 21:52:43 +0000

    Allow stepped dragging of the drawbar
    The main functional change here is to implement stepped dragging of the
    drawbar. I've introduced a Drawbar class that's now responsible for
    building the actual drawbar template when you give it a container
    element.
    
    I've also dispensed with the invisible track segments from the previous
    commit and used a calculated bottom margin on the thumb to implement the
    drawbar overflow.

--- js/drawbar.js
+++ js/drawbar.js
@@ -1,17 +1,83 @@
-const scrollIncrement = document
-  .querySelector(".drawbar-track-segment")
-  .getBoundingClientRect().height;
+const clampValue = (value) => Math.min(Math.max(value, 0), 8);
 
-let stop = 0;
+const buildDrawBar = (container) => {
+  const scroller = document.createElement("div");
+  scroller.classList.add("drawbar-scroller");
+  container.appendChild(scroller);
 
-const move = () => {
-  stop = (stop + 1) % 9;
-  document
-    .querySelector(".drawbar-container")
-    .scrollTo({ top: stop * scrollIncrement, behaviour: "instant" });
-  window.setTimeout(() => {
-    move();
-  }, 1000);
+  const track = document.createElement("div");
+  track.classList.add("drawbar-track");
+  scroller.appendChild(track);
+
+  const thumb = document.createElement("div");
+  thumb.classList.add("drawbar-thumb");
+  scroller.appendChild(thumb);
+
+  for (var i = 0; i < 8; i++) {
+    const segment = document.createElement("div");
+    segment.classList.add("drawbar-track-segment");
+    segment.innerText = 8 - i;
+    track.appendChild(segment);
+  }
 };
 
-move();
+class Drawbar {
+  constructor(container) {
+    this.container = container;
+    buildDrawBar(container);
+    this.segmentHeight = container
+      .querySelector(".drawbar-track-segment")
+      .getBoundingClientRect().height;
+    this.value = 8;
+    this.dragging = false;
+    this.container.addEventListener("mousedown", (e) => this.startDrag(e));
+    this.container.addEventListener("mousemove", (e) => this.move(e));
+    this.container.addEventListener("mouseleave", (e) => this.stopDrag(e));
+    this.container.addEventListener("mouseup", (e) => this.stopDrag(e));
+  }
+
+  startDrag(e) {
+    if (!(e.buttons & 1)) {
+      return true;
+    }
+
+    this.dragging = true;
+    this.startY = e.pageY;
+    this.startValue = this.value;
+    this.container.classList.add("dragging");
+  }
+
+  stopDrag(e) {
+    this.dragging = false;
+    this.startY = null;
+    this.startValue = null;
+    this.container.classList.remove("dragging");
+  }
+
+  move(e) {
+    if (!this.dragging) {
+      return;
+    }
+
+    const pixelsMoved = this.startY - e.pageY;
+    const newValue = clampValue(
+      this.startValue - Math.floor(pixelsMoved / this.segmentHeight)
+    );
+
+    if (newValue !== this.value) {
+      this.setValue(newValue);
+    }
+  }
+
+  setValue(value) {
+    this.value = clampValue(value);
+
+    this.container.scrollTo({
+      top: Math.ceil((8 - this.value) * this.segmentHeight),
+      behaviour: "instant",
+    });
+  }
+}
+
+document.querySelectorAll(".drawbar").forEach((el) => new Drawbar(el));
+// new Drawbar(document.querySelector(".drawbar"));

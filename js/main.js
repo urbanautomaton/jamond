f93d61ea8a9ab00d6c6da1cad4201bda5e1a5261
Simon Coffey <simon.coffey@futurelearn.com>
2023-03-03 17:44:55 +0000

    Extract KeyboardHammondInput
    This extracts the computer keyboard input management, following the
    pattern established for the MIDI input.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import Synth from "./modules/synth.js";
 import Drawbar from "./modules/drawbar.js";
 import MidiHammondInput from "./modules/midi_hammond_input.js";
+import KeyboardHammondInput from "./modules/keyboard_hammond_input.js";
 
 const keyboard = document.getElementById("keyboard");
 const drawbars = keyboard.querySelector(".drawbars");
@@ -64,40 +65,5 @@ synth.eachManualKey(({ midiNote, octave, name }) => {
   keys.appendChild(keyElement);
 });
 
-const keyMap = {
-  a: { midiNote: 60 },
-  w: { midiNote: 61 },
-  s: { midiNote: 62 },
-  e: { midiNote: 63 },
-  d: { midiNote: 64 },
-  f: { midiNote: 65 },
-  t: { midiNote: 66 },
-  g: { midiNote: 67 },
-  y: { midiNote: 68 },
-  h: { midiNote: 69 },
-  u: { midiNote: 70 },
-  j: { midiNote: 71 },
-  k: { midiNote: 72 },
-};
-
-document.addEventListener(
-  "keydown",
-  (e) => {
-    if (keyMap.hasOwnProperty(e.key)) {
-      playMidiNote(keyMap[e.key].midiNote);
-    }
-  },
-  false
-);
-
-document.addEventListener(
-  "keyup",
-  (e) => {
-    if (keyMap.hasOwnProperty(e.key)) {
-      stopMidiNote(keyMap[e.key].midiNote);
-    }
-  },
-  false
-);
-
+new KeyboardHammondInput({ playMidiNote, stopMidiNote });
 new MidiHammondInput({ playMidiNote, stopMidiNote });

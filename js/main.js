2312e0000b5a2997352f7d45a416b9e98ae047a0
Simon Coffey <simon.coffey@futurelearn.com>
2023-03-03 18:31:34 +0000

    Extract Controller module
    Just can't stop extractin' I guess

--- js/main.js
+++ js/main.js
@@ -1,39 +1,9 @@
-import { ManualKeys, Drawbars, isManualKey } from "./modules/definitions.js";
+import { Drawbars } from "./modules/definitions.js";
+import Controller from "./modules/controller.js";
 import Synth from "./modules/synth.js";
 import MidiHammondInput from "./modules/midi_hammond_input.js";
 import KeyboardHammondInput from "./modules/keyboard_hammond_input.js";
 import HammondUI from "./modules/hammond_ui.js";
-
-class Controller {
-  constructor() {
-    this.handlers = {};
-  }
-
-  on(event, cb) {
-    this.handlers[event] ||= [];
-    this.handlers[event].push(cb);
-  }
-
-  trigger(event, ...args) {
-    (this.handlers[event] || []).forEach((cb) => cb(...args));
-  }
-
-  playMidiNote(midiNote) {
-    if (isManualKey(midiNote)) {
-      this.trigger("playmidinote", midiNote);
-    }
-  }
-
-  stopMidiNote(midiNote) {
-    if (isManualKey(midiNote)) {
-      this.trigger("stopmidinote", midiNote);
-    }
-  }
-
-  setDrawbar(index, value) {
-    this.trigger("setdrawbar", index, value);
-  }
-}
 
 const controller = new Controller();
 
---
+++ js/modules/controller.js
@@ -0,0 +1,34 @@
+import { isManualKey } from "./definitions.js";
+
+class Controller {
+  constructor() {
+    this.handlers = {};
+  }
+
+  on(event, cb) {
+    this.handlers[event] ||= [];
+    this.handlers[event].push(cb);
+  }
+
+  trigger(event, ...args) {
+    (this.handlers[event] || []).forEach((cb) => cb(...args));
+  }
+
+  playMidiNote(midiNote) {
+    if (isManualKey(midiNote)) {
+      this.trigger("playmidinote", midiNote);
+    }
+  }
+
+  stopMidiNote(midiNote) {
+    if (isManualKey(midiNote)) {
+      this.trigger("stopmidinote", midiNote);
+    }
+  }
+
+  setDrawbar(index, value) {
+    this.trigger("setdrawbar", index, value);
+  }
+}
+
+export default Controller;

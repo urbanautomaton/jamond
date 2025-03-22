b8aeef484b19cd49406018e7a9a19d951316d7e6
Simon Coffey <simon@urbanautomaton.com>
2025-03-22 14:48:07 +0000

    Rework page layout a bit
    I want to think about making this responsive (it's currently not at
    all), which this commit doesn't do at all but does switch to an overall
    flex layout I think it'll be easier to work within, and centres
    everything. I'm also making the whole page brown cos the white-on-brown
    rectangle didn't look great.

--- js/modules/hammond_ui.js
+++ js/modules/hammond_ui.js
@@ -20,80 +20,82 @@ class HammondUI {
     this.container = container;
     this.controller = controller;
 
-    this.container.div({ className: "vibratoControls" }, (vibratoControls) => {
-      vibratoControls.input(
-        { name: "vibratoEnabled", type: "checkbox", checked: true },
-        (vibratoEnabled) => {
-          vibratoEnabled.onchange = (event) => {
-            this.controller.enableVibrato(event.target.checked);
-          };
-        }
-      );
-
-      vibratoControls.label({
-        for: "vibratoEnabled",
-        innerText: "Vibrato enabled?",
-      });
-
-      vibratoControls.select(
-        { name: "vibratoMode", required: true },
-        (vibratoMode) => {
-          VibratoModes.forEach((mode) => {
-            vibratoMode.option({ value: mode, innerText: mode });
-          });
-          vibratoMode.onchange = (event) => {
-            this.controller.setVibratoMode(event.target.value);
-          };
-          vibratoMode.value = "C-3";
-        }
-      );
-
-      vibratoControls.label({
-        for: "vibratoMode",
-        innerText: "Vibrato Mode",
-      });
-    });
-
-    this.container.div({ className: "drawbars" }, (drawbars) => {
-      Drawbars.forEach(({ label, color }, index) => {
-        drawbars.div({ className: `drawbar ${color}` }, (drawbar) => {
-          new Drawbar(drawbar, label, index, controller);
-        });
-      });
-    });
-
-    this.container.div({ className: "keys" }, (keys) => {
-      this.controller.on("playmidinote", (midiNote) => {
-        keys
-          .querySelector(`[data-midi-note="${midiNote}"]`)
-          .classList.add("pressed");
-      });
-
-      this.controller.on("stopmidinote", (midiNote) => {
-        keys
-          .querySelector(`[data-midi-note="${midiNote}"]`)
-          .classList.remove("pressed");
-      });
-
-      ManualKeys.forEach(({ midiNote, octave, name }) => {
-        const keyClassName = name.toLowerCase().replaceAll("#", "s");
-        const keyColor = name.length > 1 ? "black" : "white";
-
-        keys.div(
-          { className: `key ${keyClassName} ${keyColor}` },
-          (keyElement) => {
-            Object.assign(keyElement.dataset, { midiNote });
-
-            keyElement.onmousedown = (e) => this.onKeyDown(e);
-            keyElement.onmouseenter = (e) => this.onKeyDown(e);
-            keyElement.onmouseup = (e) => this.onKeyUp(e);
-            keyElement.onmouseleave = (e) => this.onKeyUp(e);
-
-            keyElement.div({ className: "label" }, (labelElement) => {
-              labelElement.innerHTML = `${name}<sub>${octave}</sub>`;
-            });
+    this.container.div({ className: "controls" }, (controls) => {
+      controls.div({ className: "vibratoControls" }, (vibratoControls) => {
+        vibratoControls.input(
+          { name: "vibratoEnabled", type: "checkbox", checked: true },
+          (vibratoEnabled) => {
+            vibratoEnabled.onchange = (event) => {
+              this.controller.enableVibrato(event.target.checked);
+            };
           }
         );
+
+        vibratoControls.label({
+          for: "vibratoEnabled",
+          innerText: "Vibrato enabled?",
+        });
+
+        vibratoControls.select(
+          { name: "vibratoMode", required: true },
+          (vibratoMode) => {
+            VibratoModes.forEach((mode) => {
+              vibratoMode.option({ value: mode, innerText: mode });
+            });
+            vibratoMode.onchange = (event) => {
+              this.controller.setVibratoMode(event.target.value);
+            };
+            vibratoMode.value = "C-3";
+          }
+        );
+
+        vibratoControls.label({
+          for: "vibratoMode",
+          innerText: "Vibrato Mode",
+        });
+      });
+
+      controls.div({ className: "drawbars" }, (drawbars) => {
+        Drawbars.forEach(({ label, color }, index) => {
+          drawbars.div({ className: `drawbar ${color}` }, (drawbar) => {
+            new Drawbar(drawbar, label, index, controller);
+          });
+        });
+      });
+
+      controls.div({ className: "keys" }, (keys) => {
+        this.controller.on("playmidinote", (midiNote) => {
+          keys
+            .querySelector(`[data-midi-note="${midiNote}"]`)
+            .classList.add("pressed");
+        });
+
+        this.controller.on("stopmidinote", (midiNote) => {
+          keys
+            .querySelector(`[data-midi-note="${midiNote}"]`)
+            .classList.remove("pressed");
+        });
+
+        ManualKeys.forEach(({ midiNote, octave, name }) => {
+          const keyClassName = name.toLowerCase().replaceAll("#", "s");
+          const keyColor = name.length > 1 ? "black" : "white";
+
+          keys.div(
+            { className: `key ${keyClassName} ${keyColor}` },
+            (keyElement) => {
+              Object.assign(keyElement.dataset, { midiNote });
+
+              keyElement.onmousedown = (e) => this.onKeyDown(e);
+              keyElement.onmouseenter = (e) => this.onKeyDown(e);
+              keyElement.onmouseup = (e) => this.onKeyUp(e);
+              keyElement.onmouseleave = (e) => this.onKeyUp(e);
+
+              keyElement.div({ className: "label" }, (labelElement) => {
+                labelElement.innerHTML = `${name}<sub>${octave}</sub>`;
+              });
+            }
+          );
+        });
       });
     });
   }
